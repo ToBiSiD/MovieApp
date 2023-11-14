16550d351f4398dfd0644816aa509173cf8d788c
VanyaTobiasFate <i.kucherenko@outloud.games>
2023-11-14 20:56:33 +0200

    Initial Commit

---
+++ IosTestTask_iMakeBetter/IosTestTask_iMakeBetterApp.swift
@@ -0,0 +1,17 @@
+//
+//  IosTestTask_iMakeBetterApp.swift
+//  IosTestTask_iMakeBetter
+//
+//  Created by Tobias on 14.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct IosTestTask_iMakeBetterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
